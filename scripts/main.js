cdda3f7646c2113bb98d80ae4f1dfe77b35742fc
Christopher Kemp <cmkemp52@gmail.com>
2019-09-24 09:21:26 -0400

    coded in class

--- scripts/main.js
+++ scripts/main.js
@@ -2,19 +2,19 @@
 
 const getMoreQuotesButton = document.getElementById("getMoreQuotes");
 const chuckSays = document.getElementById("chuckSays")
+const chuckImage = document.getElementById("chuckImage")
 
 // Add an event listener to the button, DON'T FORGET TO PREVENT THE DEFAULT BEHAVIOR!
 // Call a function to return a new quote, and update the DOM
 getMoreQuotesButton.addEventListener("click", function(e) {
-    console.log("Buttpress");
-    updateChuckSays();
+    console.log("Butpress");
+    updateChuckSays("dev");
 });
 
 // Create a function to update the quote text in the DOM
-function updateChuckSays() {
-    fetch("https://api.chucknorris.io/jokes/random?category=food")
-        .then(response => response.json())
-        .then(data => chuckSays.innerHTML = data.value);
-    chuckSays.innerHTML = "...";
+function updateChuckSays(category) {
+    get(`https://api.chucknorris.io/jokes/random?category=${category}`)
+        .then((quote) => {chuckSays.innerHTML = quote;});
+    chuckSays.innerHTML = "..."
 }
-updateChuckSays();
+updateChuckSays("food");
